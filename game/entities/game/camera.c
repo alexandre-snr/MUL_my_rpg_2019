25e0f856c756b9d580748c589b0e4fda42feb62b
Alexandre Sauner <alexandre.sauner@epitech.eu>
2020-04-03 10:32:52 +0200

    add: clamp camera to background

--- game/entities/game/camera.c
+++ game/entities/game/camera.c
@@ -19,11 +19,19 @@ static void draw(entity_t *self, engine_t *engine)
 {
     entity_player_data_t *pdata =
     snr_scene_get_entity(engine->sm->scene, "Player")->data;
-    sfVector2u win_size = sfRenderWindow_getSize(engine->win);
+    entity_background_data_t *bdata =
+    snr_scene_get_entity(engine->sm->scene, "Bg")->data;
+    sfVector2u bg_size = sfTexture_getSize(bdata->texture);
+    sfVector2u w_size = sfRenderWindow_getSize(engine->win);
+    sfVector2f pos = pdata->pos;
     sfView *view = sfView_create();
 
-    sfView_setSize(view, snr_create_vector2f(win_size.x, win_size.y));
-    sfView_setCenter(view, pdata->pos);
+    pos.x = pos.x < w_size.x / 2 ? w_size.x / 2 : pos.x;
+    pos.x = pos.x > bg_size.x - w_size.x / 2 ? bg_size.x - w_size.x / 2 : pos.x;
+    pos.y = pos.y < w_size.y / 2 ? w_size.y / 2 : pos.y;
+    pos.y = pos.y > bg_size.y - w_size.y / 2 ? bg_size.y - w_size.y / 2 : pos.y;
+    sfView_setSize(view, snr_create_vector2f(w_size.x, w_size.y));
+    sfView_setCenter(view, pos);
     sfRenderWindow_setView(engine->win, view);
 }
 
