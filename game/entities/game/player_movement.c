ae8a114c0f777de61a8760f75d1402a2a50254e7
Alexandre Sauner <alexandre.sauner@epitech.eu>
2020-04-10 17:41:43 +0200

    fix: player spritesheets

--- game/entities/game/player_movement.c
+++ game/entities/game/player_movement.c
@@ -14,21 +14,19 @@ static void animation(entity_player_data_t *data, sfVector2f *in, engine_t *en)
 {
     data->rect.top = in->y < 0 ? data->rect.height * 3 : data->rect.top;
     data->rect.top = in->y > 0 ? data->rect.height * 0 : data->rect.top;
-    data->rect.top = in->x < 0 ? data->rect.height * 1 : data->rect.top;
-    data->rect.top = in->x > 0 ? data->rect.height * 2 : data->rect.top;
+    data->rect.top = in->x < 0 ? data->rect.height * 2 : data->rect.top;
+    data->rect.top = in->x > 0 ? data->rect.height * 1 : data->rect.top;
     if (snr_get_len(in) == 0 || en->dt->val == 0.0f)
         data->rect.left = 0;
     else
-        data->rect.left = ((int) (en->dt->since_start * 10) % 3)
+        data->rect.left = ((int) (en->dt->since_start * 10) % 4)
         * data->rect.width;
 }
 
-void player_movement(entity_t *self, engine_t *engine)
+static sfVector2f get_input(void)
 {
-    DATA(player);
     sfVector2f input = {0, 0};
     float input_len = 0;
-    sfFloatRect rect = {0, 0, data->rect.width, data->rect.height / 2};
 
     input.x += sfKeyboard_isKeyPressed(sfKeyQ) ? -1 : 0;
     input.x += sfKeyboard_isKeyPressed(sfKeyD) ? 1 : 0;
@@ -39,13 +37,24 @@ void player_movement(entity_t *self, engine_t *engine)
         input.x /= input_len;
         input.y /= input_len;
     }
-    data->pos.x += input.x * 200 * engine->dt->val;
-    data->pos.y += input.y * 200 * engine->dt->val;
+    return (input);
+}
+
+void player_movement(entity_t *self, engine_t *engine)
+{
+    DATA(player);
+    sfFloatRect rect = {0, 0, data->rect.width, data->rect.height / 2};
+    sfVector2f input = get_input();
+    const float speed = 200;
+
+
+    data->pos.x += input.x * speed * engine->dt->val;
+    data->pos.y += input.y * speed * engine->dt->val;
     rect.left = data->pos.x;
     rect.top = data->pos.y + data->rect.height / 2;
     if (check_colliders(engine, &rect)) {
-        data->pos.x -= input.x * 200 * engine->dt->val;
-        data->pos.y -= input.y * 200 * engine->dt->val;
+        data->pos.x -= input.x * speed * engine->dt->val;
+        data->pos.y -= input.y * speed * engine->dt->val;
     }
     animation(data, &input, engine);
     sfSprite_setTextureRect(data->sprite, data->rect);
