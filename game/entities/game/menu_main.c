ef0fa4f7c4311fa0c71e260d9e271b255b27ab4c
Alexandre Sauner <alexandre.sauner@epitech.eu>
2020-04-13 16:57:31 +0200

    add: save and quit in I menu

--- game/entities/game/menu_main.c
+++ game/entities/game/menu_main.c
@@ -10,24 +10,34 @@
 #include "entities.h"
 #include "entities_data.h"
 #include "inventory.h"
+#include "save.h"
+#include "scene_manager.h"
+#include "scenes.h"
 #include "string_utils.h"
 #include "string_convert.h"
 #include <stdlib.h>
 
+static void quit(engine_t *engine)
+{
+    save(engine);
+    snr_scene_manager_load(engine, create_scene_main_menu(engine));
+}
+
 void open_main_menu(engine_t *engine, int bypass_key)
 {
     menu_entry_t **entries;
-    char *texts[] = {"MENU:", "Inventaire", "Stats du joueur"};
-    void *callbacks[] = {NULL, open_inventory, open_stats};
+    char *texts[] = {"MENU:", "Inventaire", "Stats du joueur", "Sauvegarder",
+    "Quitter"};
+    void *callbacks[] = {NULL, open_inventory, open_stats, save, quit};
 
     if (sfKeyboard_isKeyPressed(sfKeyI) || bypass_key) {
-        entries = malloc(sizeof(menu_entry_t) * 4);
-        for (int i = 0; i < 3; i++) {
+        entries = malloc(sizeof(menu_entry_t) * 6);
+        for (int i = 0; i < 5; i++) {
             entries[i] = malloc(sizeof(menu_entry_t));
             entries[i]->text = my_strdup(texts[i]);
             entries[i]->callback = callbacks[i];
         }
-        entries[3] = NULL;
+        entries[5] = NULL;
         open_menu(engine, entries);
     }
 }
